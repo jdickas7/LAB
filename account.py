4c5def6738c152d19031def7424f9bfd6d51c16f
jdickas7 <118545486+jdickas7@users.noreply.github.com>
2022-11-18 10:41:32 -0600

    Add files via upload

---
+++ account.py
@@ -0,0 +1,23 @@
+class Account:
+    def __init__(self, name):
+        self.account_name = name
+        self.account_balance = 0
+
+    def deposit(self, amount):
+        if float(amount) > 0:
+            self.account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if float(amount) > 0 and self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.account_balance
+    def get_name(self):
+        return self.account_name
